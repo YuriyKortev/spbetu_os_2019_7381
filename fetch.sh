e983d387d90e72a7331764013bdb6fcd99c932a0
JAkutenshi <jakutenshi@gmail.com>
2019-03-26 17:27:26 +0300

    Add git scripts for.. 7 students at all? R U kiddin' me?!

---
+++ fetch.sh
@@ -0,0 +1,7 @@
+git fetch dorokh
+git fetch sudakova
+git fetch aliasova
+git fetch vologdin
+git fetch trushnikov
+git fetch gabov
+git fetch procvetkina
---
+++ remotes.sh
@@ -0,0 +1,7 @@
+git remote add dorokh https://github.com/PrimeBR/spbetu_os_2019_7381.git
+git remote add sudakova https://github.com/polinasudakova/spbetu_os_2019_7381.git
+git remote add aliasova https://github.com/Aliaska-An/spbetu_os_2019_7381.git
+git remote add vologdin https://github.com/Makkksx/spbetu_os_2019_7381.git
+git remote add trushnikov https://github.com/AndersonGH/spbetu_os_2019_7381.git
+git remote add gabov https://github.com/GabovYegor/spbetu_os_2019_7381.git
+git remote add procvetkina https://github.com/ana24stasiya/spbetu_os_2019_7381.git
